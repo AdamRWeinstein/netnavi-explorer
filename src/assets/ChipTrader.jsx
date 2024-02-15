0015291650258c65ca65472f310dd764e39a3ec3
AdamRWeinstein <adam.rick.weinstein@gmail.com>
2024-02-15 12:40:55 -0500

    Add shop assets

---
+++ src/assets/ChipTrader.jsx
@@ -0,0 +1,54 @@
+const ChipTrader = () => {
+    return (
+        <>
+            <rect className="svg-clickable-area" x="8" y="0" width="6" height="1" fill="black" />
+            <rect className="svg-clickable-area" x="6" y="1" width="10" height="1" fill="black" />
+            <rect className="svg-clickable-area" x="5" y="2" width="12" height="1" fill="black" />
+            <rect className="svg-clickable-area" x="4" y="3" width="14" height="1" fill="black" />
+            <rect className="svg-clickable-area" x="3" y="4" width="16" height="1" fill="black" />
+            <rect className="svg-clickable-area" x="3" y="5" width="16" height="1" fill="black" />
+            <rect className="svg-clickable-area" x="2" y="6" width="18" height="1" fill="black" />
+            <rect className="svg-clickable-area" x="2" y="7" width="18" height="1" fill="black" />
+            <rect className="svg-clickable-area" x="2" y="8" width="18" height="1" fill="black" />
+            <rect className="svg-clickable-area" x="2" y="9" width="18" height="1" fill="black" />
+            <rect className="svg-clickable-area" x="2" y="10" width="18" height="1" fill="black" />
+            <rect className="svg-clickable-area" x="3" y="11" width="16" height="1" fill="black" />
+            <rect className="svg-clickable-area" x="3" y="12" width="16" height="1" fill="black" />
+            <rect className="svg-clickable-area" x="4" y="13" width="14" height="1" fill="black" />
+            <rect className="svg-clickable-area" x="5" y="14" width="12" height="1" fill="black" />
+            <rect className="svg-clickable-area" x="5" y="15" width="12" height="1" fill="black" />
+            <rect className="svg-clickable-area" x="5" y="16" width="12" height="1" fill="black" />
+            <rect className="svg-clickable-area" x="5" y="17" width="12" height="1" fill="black" />
+            <rect className="svg-clickable-area" x="5" y="18" width="12" height="1" fill="black" />
+            <rect className="svg-clickable-area" x="5" y="19" width="12" height="1" fill="black" />
+            <rect className="svg-clickable-area" x="5" y="20" width="12" height="1" fill="black" />
+            <rect className="svg-clickable-area" x="4" y="21" width="13" height="1" fill="black" />
+            <rect className="svg-clickable-area" x="2" y="22" width="15" height="1" fill="black" />
+            <rect className="svg-clickable-area" x="0" y="23" width="17" height="1" fill="black" />
+            <rect className="svg-clickable-area" x="0" y="24" width="17" height="1" fill="black" />
+            <rect className="svg-clickable-area" x="0" y="25" width="17" height="1" fill="black" />
+            <rect className="svg-clickable-area" x="1" y="26" width="15" height="1" fill="black" />
+            <rect className="svg-clickable-area" x="2" y="27" width="13" height="1" fill="black" />
+            <rect className="svg-clickable-area" x="4" y="28" width="10" height="1" fill="black" />
+            <rect className="svg-clickable-area" x="6" y="29" width="8" height="1" fill="black" />
+            <rect className="svg-clickable-area" x="8" y="30" width="6" height="1" fill="black" />
+            <rect className="svg-clickable-area" x="5" y="31" width="12" height="1" fill="black" />
+            <rect className="svg-clickable-area" x="4" y="32" width="14" height="1" fill="black" />
+            <rect className="svg-clickable-area" x="3" y="33" width="16" height="1" fill="black" />
+            <rect className="svg-clickable-area" x="2" y="34" width="18" height="1" fill="black" />
+            <rect className="svg-clickable-area" x="2" y="35" width="18" height="1" fill="black" />
+            <rect className="svg-clickable-area" x="2" y="36" width="18" height="1" fill="black" />
+            <rect className="svg-clickable-area" x="2" y="37" width="18" height="1" fill="black" />
+            <rect className="svg-clickable-area" x="2" y="38" width="18" height="1" fill="black" />
+            <rect className="svg-clickable-area" x="2" y="39" width="18" height="1" fill="black" />
+            <rect className="svg-clickable-area" x="2" y="40" width="18" height="1" fill="black" />
+            <rect className="svg-clickable-area" x="2" y="41" width="18" height="1" fill="black" />
+            <rect className="svg-clickable-area" x="3" y="42" width="16" height="1" fill="black" />
+            <rect className="svg-clickable-area" x="4" y="43" width="14" height="1" fill="black" />
+            <rect className="svg-clickable-area" x="5" y="44" width="11" height="1" fill="black" />
+            <rect className="svg-clickable-area" x="6" y="45" width="8" height="1" fill="black" />
+        </>
+    )
+}
+
+export default ChipTrader
---
+++ src/assets/HigsbyOutline.jsx
@@ -0,0 +1,46 @@
+const HigsbyOutline = () => {
+    return (
+        <>
+            <rect className="svg-clickable-area" x="9" y="0" width="1" height="1" fill="black"/>
+            <rect className="svg-clickable-area" x="9" y="1" width="2" height="1" fill="black"/>
+            <rect className="svg-clickable-area" x="5" y="2" width="8" height="1" fill="black"/>
+            <rect className="svg-clickable-area" x="14" y="2" width="3" height="1" fill="black"/>
+            <rect className="svg-clickable-area" x="6" y="3" width="10" height="1" fill="black"/>
+            <rect className="svg-clickable-area" x="4" y="4" width="13" height="1" fill="black"/>
+            <rect className="svg-clickable-area" x="2" y="5" width="16" height="1" fill="black"/>
+            <rect className="svg-clickable-area" x="3" y="6" width="18" height="1" fill="black"/>
+            <rect className="svg-clickable-area" x="2" y="7" width="18" height="1" fill="black"/>
+            <rect className="svg-clickable-area" x="0" y="8" width="21" height="1" fill="black"/>
+            <rect className="svg-clickable-area" x="1" y="9" width="21" height="1" fill="black"/>
+            <rect className="svg-clickable-area" x="2" y="10" width="19" height="1" fill="black"/>
+            <rect className="svg-clickable-area" x="3" y="11" width="17" height="1" fill="black"/>
+            <rect className="svg-clickable-area" x="3" y="12" width="15" height="1" fill="black"/>
+            <rect className="svg-clickable-area" x="6" y="13" width="12" height="1" fill="black"/>
+            <rect className="svg-clickable-area" x="7" y="14" width="9" height="1" fill="black"/>
+            <rect className="svg-clickable-area" x="7" y="15" width="6" height="1" fill="black"/>
+            <rect className="svg-clickable-area" x="7" y="16" width="7" height="1" fill="black"/>
+            <rect className="svg-clickable-area" x="7" y="17" width="8" height="1" fill="black"/>
+            <rect className="svg-clickable-area" x="7" y="18" width="9" height="1" fill="black"/>
+            <rect className="svg-clickable-area" x="7" y="19" width="9" height="1" fill="black"/>
+            <rect className="svg-clickable-area" x="7" y="20" width="9" height="1" fill="black"/>
+            <rect className="svg-clickable-area" x="6" y="21" width="11" height="1" fill="black"/>
+            <rect className="svg-clickable-area" x="6" y="22" width="11" height="1" fill="black"/>
+            <rect className="svg-clickable-area" x="6" y="23" width="11" height="1" fill="black"/>
+            <rect className="svg-clickable-area" x="5" y="24" width="12" height="1" fill="black"/>
+            <rect className="svg-clickable-area" x="5" y="25" width="13" height="1" fill="black"/>
+            <rect className="svg-clickable-area" x="5" y="26" width="13" height="1" fill="black"/>
+            <rect className="svg-clickable-area" x="5" y="27" width="13" height="1" fill="black"/>
+            <rect className="svg-clickable-area" x="4" y="28" width="14" height="1" fill="black"/>
+            <rect className="svg-clickable-area" x="4" y="29" width="14" height="1" fill="black"/>
+            <rect className="svg-clickable-area" x="5" y="30" width="13" height="1" fill="black"/>
+            <rect className="svg-clickable-area" x="7" y="31" width="11" height="1" fill="black"/>
+            <rect className="svg-clickable-area" x="7" y="32" width="11" height="1" fill="black"/>
+            <rect className="svg-clickable-area" x="9" y="33" width="9" height="1" fill="black"/>
+            <rect className="svg-clickable-area" x="11" y="34" width="6" height="1" fill="black"/>
+            <rect className="svg-clickable-area" x="13" y="35" width="4" height="1" fill="black"/>
+            <rect className="svg-clickable-area" x="14" y="36" width="1" height="1" fill="black"/>
+        </>
+    )
+}
+
+export default HigsbyOutline
