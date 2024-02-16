1e45580b7a9d020fbd3a73a756488f4bbcef7d61
AdamRWeinstein <adam.rick.weinstein@gmail.com>
2024-02-16 08:02:23 -0500

    Add chip roulette

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,13 +1,45 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
 import ClickableRegion from "./ClickableRegion";
 import ShopInterior from '../assets/ShopInterior.png'
 import HigsbyOutline from '../assets/HigsbyOutline'
 import ChipTrader from '../assets/ChipTrader'
 import { useNavigate } from "react-router-dom";
+import SuperGif from 'libgif'
 
 const Map = () => {
     const navigate = useNavigate();
+    const [gifVisible, setGifVisible] = useState(false)
+    const gifRef = useRef(null)
+
+    useEffect(() => {
+        if (gifVisible && gifRef.current) {
+            let gif = new SuperGif({ 
+                gif: gifRef.current,
+                auto_play: 0,
+                progressbar_height: '100%'
+            });
+            gif.load(() => {
+                let speed = "fast"
+                let final = Math.floor(Math.random() * 10) + 50
+                const moveToRandomFrame = (count = 0) => {
+                    if (count === final) return; 
+                    if (count === 30) speed = "medium"
+                    if (count === 45) speed = "slow"
+                    if (count === final - 2) speed = "slowest"
+    
+                    const totalFrames = gif.get_length(); 
+                    const randomFrame = Math.floor(Math.random() * totalFrames); 
+                    gif.move_to(randomFrame);
+                    setTimeout(() => moveToRandomFrame(count + 1), speed === "fast" ? 100 : speed === "medium" ? 200 : speed === "slow" ? 500 : 1000);
+                };
+    
+                moveToRandomFrame();
+            });
+        }
+    }, [gifVisible]);
+
+    const spinChip = () => {setGifVisible(true)}
 
     return (
         <div className="shop-container">
@@ -40,7 +72,7 @@ const Map = () => {
                                 id="ChipTrader"
                                 SVG={ChipTrader}
                                 width='20px'
-                                onClick={() => navigate('/folders/chip-trader')}
+                                onClick={() => spinChip()}
                                 style={{
                                     position: 'absolute',
                                     left: '195px',
@@ -56,6 +88,15 @@ const Map = () => {
                     </React.Fragment>
                 )}
             </TransformWrapper>
+            {gifVisible && (
+                <img
+                    className="chip-trader-roulette"
+                    ref={gifRef}
+                    id="chipGif"
+                    src="/assets/BattleChips/ChipTrader/chipTraderSpin.gif"
+                    alt="Spinning Chip"
+                />
+            )}
         </div>
     )
 }
